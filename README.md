a94d095c15bfb5db86bda3716ed33e4ae898302c
RamVetri <vetriram99@gmail.com>
2023-12-07 14:24:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## What is this?
+
+Basic Arithmetic Operations
+
+
+## Installation
+
+"npm install react-native-mathematics"
+
+Use:
+
+...
+
+import {add} from 'react-native-mathematics'
+
+const num = add(5,4)
+
+print (num)
+
+...
+
+## Parameter
+
+This package has 4 basic arithmetic operations functions (add, sub, mul, div),
+
+each accepts 2 parameter and they returns the result.
---
+++ index.js
@@ -0,0 +1,24 @@
+export function add (num1 , num2) {
+    const result = num1 + num2
+    return result
+}
+
+export function sub (num1 , num2) {
+    const result = num1 - num2
+    return result
+}
+
+export function mul (num1 , num2) {
+    const result = num1 * num2
+    return result
+}
+
+export function div (num1 , num2) {
+    const result = num1 / num2
+    return result
+}
+
+export function modulus (num1 , num2) {
+    const result = num1 % num2
+    return result
+}
